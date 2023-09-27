cadd78d66b90a5e16e44b33eb414e6007bb645ab
Nick Gerner <nick.gerner@gmail.com>
2023-09-27 16:38:53 -0700

    set up a menu system for controlling the analyzer

---
+++ zeroii-analyzer/menu_manager.h
@@ -0,0 +1,95 @@
+#ifndef _MENU_MANAGER_H
+#define _MENU_MANAGER_H
+
+#include<assert.h>
+
+struct Menu;
+
+struct MenuOption {
+    MenuOption(String l, int oid, Menu* sm) {
+        label = l;
+        option_id = oid;
+        sub_menu = sm;
+    }
+    String label;
+    int option_id;
+    Menu* sub_menu;
+};
+
+struct Menu {
+    Menu(Menu* p, MenuOption* o, size_t o_count) {
+        parent = p;
+        options = o;
+        option_count = o_count;
+        selected_option = 0;
+
+        //set us as parent
+        for (size_t i=0; i<option_count; i++) {
+            if (options[i].sub_menu != NULL) {
+                options[i].sub_menu->parent = this;
+            }
+        }
+    }
+
+    Menu* parent;
+    MenuOption* options;
+    size_t option_count;
+    size_t selected_option;
+};
+
+class MenuManager {
+    public:
+        MenuManager(Menu* root_menu) {
+            root_menu_ = root_menu;
+            current_menu_ = root_menu;
+            current_option_ = -1;
+        }
+
+        void select(size_t i) {
+            if (i < current_menu_->option_count) {
+                current_menu_->selected_option = i;
+            }
+        }
+
+        void select_up() {
+            select(current_menu_->selected_option-1);
+        }
+
+        void select_down() {
+            select(current_menu_->selected_option+1);
+        }
+
+        void expand() {
+            assert(current_menu_->selected_option < current_menu_->option_count);
+            MenuOption* current_option = &(current_menu_->options[current_menu_->selected_option]);
+            if (current_option_ >= 0) {
+                return;
+            }
+            if (current_option->sub_menu == NULL) {
+                current_option_ = current_option->option_id;
+            } else {
+                current_menu_ = current_option->sub_menu;
+            }
+        }
+
+        void collapse() {
+            if (current_option_ >= 0) {
+                current_option_ = -1;
+            } else {
+                if (current_menu_ == root_menu_) {
+                    return;
+                }
+                assert(current_menu_->parent != NULL);
+                current_menu_->selected_option = 0;
+                current_menu_ = current_menu_->parent;
+            }
+        }
+
+        Menu* root_menu_;
+        Menu* current_menu_;
+        int current_option_;
+
+    private:
+};
+
+#endif
--- zeroii-analyzer/zeroii-analyzer.ino
+++ zeroii-analyzer/zeroii-analyzer.ino
@@ -4,6 +4,7 @@
 
 #include "analyzer.h"
 #include "rotary_encoder.h"
+#include "menu_manager.h"
 
 #define ZEROII_Reset_Pin  6
 #define ZERO_I2C_ADDRESS 0x5B
@@ -14,6 +15,156 @@
 
 Analyzer analyzer(Z0);
 RotaryEncoder encoder(CLK, DT, SW);
+
+#define MOPT_ANALYZE 1
+#define MOPT_FQ 2
+#define MOPT_CALIBRATE 3
+#define MOPT_FQCENTER 4
+#define MOPT_FQWINDOW 5
+#define MOPT_FQSTEPS 6
+#define MOPT_BACK 7
+
+MenuOption fq_menu_options[] = {
+    MenuOption("Fq Center", MOPT_FQCENTER, NULL),
+    MenuOption("Fq Range", MOPT_FQWINDOW, NULL),
+    MenuOption("Steps", MOPT_FQSTEPS, NULL),
+    MenuOption("Back", MOPT_BACK, NULL),
+};
+Menu fq_menu(NULL, fq_menu_options, sizeof(fq_menu_options)/sizeof(fq_menu_options[0]));
+
+MenuOption root_menu_options[] = {
+    MenuOption("Analyze", MOPT_ANALYZE, NULL),
+    MenuOption("Fq Options", MOPT_FQ, &fq_menu),
+    MenuOption("Calibration", MOPT_CALIBRATE, NULL),
+};
+Menu root_menu(NULL, root_menu_options, sizeof(root_menu_options)/sizeof(root_menu_options[0]));
+
+MenuManager menu_manager(&root_menu);
+
+int32_t centerFq = 28400000; //300000000;// 148 000 000 Hz
+int32_t rangeFq = 800000;//400000000;// 10 000 000 Hz
+int32_t dotsNumber = 100;
+
+#define CAL_START 0
+#define CAL_S 1
+#define CAL_O 2
+#define CAL_L 3
+#define CAL_END 4
+int calibration_state = CAL_START;
+
+void draw_menu(Menu* current_menu, int current_option) {
+    Serial.println(menu_manager.current_option_);
+    for(int i=0; i<current_menu->option_count; i++) {
+        // either:
+        // this is the current option
+        // this is the selected option
+        // this is just an option
+        if (current_option >= 0 && current_menu->options[i].option_id == current_option) {
+            Serial.print(" +");
+        } else if(current_menu->selected_option == i) {
+            Serial.print(" >");
+        } else {
+            Serial.print("  ");
+        }
+        Serial.print("[");
+        Serial.print(current_menu->options[i].label);
+        Serial.print("]");
+    }
+    Serial.println();
+}
+
+void menu_back() {
+    menu_manager.collapse();
+    draw_menu(menu_manager.current_menu_, menu_manager.current_option_);
+}
+
+void handle_waiting() {
+    switch(menu_manager.current_option_) {
+        case -1:
+            // clicking does whatever the cursor is on in the menu
+            // we'll handle that action on the next loop
+            if (encoder.click_) {
+                menu_manager.expand();
+                draw_menu(menu_manager.current_menu_, menu_manager.current_option_);
+            }
+            if (encoder.turn_ != 0) {
+                if(encoder.turn_ < 0) {
+                    menu_manager.select_down();
+                    draw_menu(menu_manager.current_menu_, menu_manager.current_option_);
+                } else if(encoder.turn_ > 0) {
+                    menu_manager.select_up();
+                    draw_menu(menu_manager.current_menu_, menu_manager.current_option_);
+                }
+            }
+            break;
+        case MOPT_BACK:
+            // need to back out of being in BACK and back out of parent menu
+            menu_manager.collapse();
+            menu_back();
+            break;
+        case MOPT_ANALYZE:
+            Serial.println("Analyzing...");
+            analyze(centerFq);
+            menu_back();
+            break;
+        case MOPT_FQCENTER:
+            // clicking backs out of this option
+            if (encoder.click_) {
+                menu_back();
+            }
+            // rotating changes frequency
+            if (encoder.turn_ != 0) {
+                if(encoder.turn_ < 0) {
+                    centerFq--;
+                } else if(encoder.turn_ > 0) {
+                    centerFq++;
+                }
+                Serial.print("Center Frequency: ");
+                Serial.println(centerFq);
+            }
+            break;
+        case MOPT_FQWINDOW:
+            // clicking backs out of this option
+            if (encoder.click_) {
+                menu_back();
+            }
+            // rotating changes window size
+            if (encoder.turn_ != 0) {
+                if(encoder.turn_ < 0) {
+                    rangeFq--;
+                } else if(encoder.turn_ > 0) {
+                    rangeFq++;
+                }
+                Serial.print("Frequency Range: ");
+                Serial.println(rangeFq);
+            }
+            break;
+        case MOPT_FQSTEPS:
+            // clicking backs out of this option
+            if (encoder.click_) {
+                menu_back();
+            }
+            // rotating changes window size
+            if (encoder.turn_ != 0) {
+                if(encoder.turn_ < 0) {
+                    dotsNumber--;
+                } else if(encoder.turn_ > 0) {
+                    dotsNumber++;
+                }
+                Serial.print("Steps: ");
+                Serial.println(dotsNumber);
+            }
+            break;
+        case MOPT_CALIBRATE:
+            calibration_state = calibration_step(calibration_state);
+            if (calibration_state == CAL_END) {
+                menu_back();
+                calibration_state = CAL_START;
+            }
+            break;
+    }
+}
+
 
 void setup() {
     Serial.begin(38400);
@@ -39,70 +190,13 @@ void setup() {
     encoder.initialize();
 
     Serial.println("done");
+
+    draw_menu(menu_manager.current_menu_, menu_manager.current_option_);
 }
 
-#define STATE_CALIBRATION 0
-#define STATE_CALIBRATION_S 1
-#define STATE_CALIBRATION_O 2
-#define STATE_CALIBRATION_L 3
-#define STATE_WAITING 4
-#define STATE_MEASURING 5
-
-int state = STATE_CALIBRATION;
-int clicks = 0;
-int32_t centerFq = 28400000; //300000000;// 148 000 000 Hz
 void loop() {
     encoder.update();
-    switch(state) {
-        case STATE_CALIBRATION:
-            Serial.println("connect short and press knob");
-            state = STATE_CALIBRATION_S;
-            break;
-        case STATE_CALIBRATION_S:
-            if (encoder.click_) {
-                Serial.println(analyzer.calibrate_short(centerFq, Z0));
-                state = STATE_CALIBRATION_O;
-                Serial.println("connect open and press knob");
-            }
-            break;
-        case STATE_CALIBRATION_O:
-            if (encoder.click_) {
-                Serial.println(analyzer.calibrate_open(centerFq, Z0));
-                state = STATE_CALIBRATION_L;
-                Serial.println("connect load and press knob");
-            }
-            break;
-        case STATE_CALIBRATION_L:
-            if (encoder.click_) {
-                Serial.println(analyzer.calibrate_load(centerFq, Z0));
-                state = STATE_WAITING;
-                Serial.println("done calibrating.");
-            }
-            break;
-        case STATE_WAITING:
-            // rotating changes frequency
-            if (encoder.turn_ != 0) {
-                if(encoder.turn_ < 0) {
-                    centerFq--;
-                } else if(encoder.turn_ > 0) {
-                    centerFq++;
-                }
-                Serial.print("Center Frequency: ");
-                Serial.println(centerFq);
-            }
-            // clicking starts a measurement
-            if (encoder.click_) {
-                Serial.println("Analyzing...");
-                state = STATE_MEASURING;
-                analyze(centerFq);
-                state = STATE_WAITING;
-            }
-            break;
-    }
-    if (encoder.click_) {
-        Serial.print("click: ");
-        Serial.println(++clicks);
-    }
+    handle_waiting();
     // TODO: if we're in a measurement, allow cancelling it by clicking
     delay(1);
 }
@@ -126,8 +220,6 @@ void analyze_frequency(int32_t fq) {
 }
 
 void analyze(int32_t centerFq) {
-    int32_t rangeFq = 800000;//400000000;// 10 000 000 Hz
-    int32_t dotsNumber = 100;
 
     int32_t startFq = centerFq - (rangeFq/2);
     int32_t endFq = centerFq + (rangeFq/2);
@@ -138,6 +230,37 @@ void analyze(int32_t centerFq) {
         analyze_frequency(startFq + (stepFq*i));
     }
     Serial.print("------------------------\r\n");
+}
+
+int calibration_step(int calibration_state) {
+    switch(calibration_state) {
+        case CAL_START:
+            Serial.println("connect short and press knob");
+            return CAL_S;
+            break;
+        case CAL_S:
+            if (encoder.click_) {
+                Serial.println(analyzer.calibrate_short(centerFq, Z0));
+                Serial.println("connect open and press knob");
+                return CAL_O;
+            }
+            break;
+        case CAL_O:
+            if (encoder.click_) {
+                Serial.println(analyzer.calibrate_open(centerFq, Z0));
+                Serial.println("connect load and press knob");
+                return CAL_L;
+            }
+            break;
+        case CAL_L:
+            if (encoder.click_) {
+                Serial.println(analyzer.calibrate_load(centerFq, Z0));
+                Serial.println("done calibrating.");
+                return CAL_END;
+            }
+            break;
+    }
+    return calibration_state;
 }
 
 /*
